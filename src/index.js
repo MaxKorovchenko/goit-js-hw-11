bfd0fdaf850039575e1f00728ae53a439beb5d21
MaxKorovchenko <max.korovchenko@gmail.com>
2023-01-19 19:59:04 +0200

    add several features

--- src/index.js
+++ src/index.js
@@ -4,30 +4,54 @@ import simpleLightbox from 'simplelightbox';
 
 const form = document.querySelector('.search-form');
 const input = document.querySelector('input');
-const button = document.querySelector('button');
+const loadMoreBtn = document.querySelector('.load-more');
 const gallery = document.querySelector('.gallery');
 
-let page = 0;
+let page = 1;
+let per_page = 4;
+loadMoreBtn.hidden = true;
 
 form.addEventListener('submit', onFormSubmit);
+loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 
 async function onFormSubmit(e) {
   e.preventDefault();
-
-  page = 0;
+  page = 1;
   gallery.innerHTML = '';
 
   try {
     const data = await fetchImages();
-
-    if (data.hits.length === 0) {
+    if (data.hits.length < 1) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
+    } else {
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
     }
 
     console.log(data);
     renderImages(data.hits);
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  loadMoreBtn.hidden = false;
+}
+
+async function onLoadMoreBtnClick() {
+  try {
+    page += 1;
+
+    const data = await fetchImages();
+    renderImages(data.hits);
+
+    if (per_page * page >= data.totalHits) {
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+
+      loadMoreBtn.hidden = true;
+    }
   } catch (error) {
     console.log(error.message);
   }
@@ -36,7 +60,7 @@ async function onFormSubmit(e) {
 async function fetchImages() {
   const BASE_URL = 'https://pixabay.com/api/';
   const API_KEY = '32970845-e4fc8afb31274d73d690834b7';
-  const QUERY_PARAM = `q=${input.value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=4`;
+  const QUERY_PARAM = `q=${input.value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`;
 
   const response = await axios.get(`${BASE_URL}?key=${API_KEY}&${QUERY_PARAM}`);
 
@@ -48,7 +72,7 @@ function renderImages(data) {
     .map(
       ({ webformatURL, tags, likes, views, comments, downloads }) => `
       <div class="photo-card">
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" width="160"/>
       <div class="info">
         <p class="info-item">
           <b>Likes: ${likes}</b>
@@ -67,5 +91,5 @@ function renderImages(data) {
     )
     .join('');
 
-  gallery.innerHTML = markup;
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
